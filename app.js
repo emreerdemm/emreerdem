59c0bdc9c0064b958bd4190815b58af3813ff715
Emre Erdem <emreerdem@Emre-MBP.home>
2022-07-08 21:46:12 +0200

    some update is done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let contact = document.getElementById("contact")
+
 contact.addEventListener("click", () => {
+
+   location.href = 'mailto:info@lanparty.nl'
     
-    alert("Mail ans op info@lanparty.nl")
 })
